946a1123505a26fd775f1d965a7c3414bb133c64
Eric Schrijver <eric@ericschrijver.nl>
2014-11-05 13:27:33 +0100

    URL is configurable

--- live.sh
+++ live.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-    curl http://beta.etherpad.org/p/fbsp2013/export/txt > livegraphviz.dot
+    curl $1 > livegraphviz.dot
     dot -Tpng livegraphviz.dot -O
     sleep 5
 done
